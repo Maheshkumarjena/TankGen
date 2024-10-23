5a898cb76b88dfa18ad64e40996d52d26810da5d
Mahesh15 <maheshkumarjena46@gmail.com>
2024-10-23 08:42:00 +0530

    logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <div className="fixed top-[-20px] left-3">
+    <div className="fixed top-1 left-3">
       <Image
         className="object-cover inset-0 z-[2] opacity-[3]  rounded-lg shadow-md dark:shadow-lg "
-        src="/tankgenLogo.jpg"
+        src="/tgLogo.jpg"
         width={190}
         height={80}
         alt="Picture of the author"
