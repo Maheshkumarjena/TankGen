70b72dcb5e214eaa0f1cf3bdf7cf6d0e091dd94b
Mahesh15 <maheshkumarjena46@gmail.com>
2024-10-23 08:46:29 +0530

    contact deleted

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import Service from "@/components/service";
 import AboutUs from "@/components/AboutUs";
 import Footer from "@/components/Footer";
 import Logo from "@/components/Logo";
-import Contact from "@/components/contact";
 export default function Home() {
   return (
    <div className="h-fit overflow-x-hidden pb-10">
